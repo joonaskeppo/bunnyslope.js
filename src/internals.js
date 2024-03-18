423c9fecdf72e8325a5197f3eee379549e4c4b46
Joonas Keppo <joonaskeppo@gmail.com>
2024-03-18 18:18:30 +0200

    Add missing constants

---
+++ src/internals.js
@@ -0,0 +1,7 @@
+export const PREFIX = 'b-';
+const INTERNAL_PREFIX = `__${PREFIX}`;
+
+export const REIFICATION = `${INTERNAL_PREFIX}reification`;
+export const TEMPLATE = `${INTERNAL_PREFIX}template`;
+export const INDEX = `${INTERNAL_PREFIX}index`;
+export const BINDING = `${INTERNAL_PREFIX}binding`;
